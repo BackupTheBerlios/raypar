917f2245bc421631860c408f9182451d1d7a8cc5
vivid <vivid>
2004-01-14 17:18:36 +0000

    some server modification

---
+++ dirt/server/LogWnd.cpp
@@ -0,0 +1,68 @@
+// LogWnd.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "server.h"
+#include "LogWnd.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CLogWnd dialog
+
+
+CLogWnd::CLogWnd(CWnd* pParent /*=NULL*/)
+	: CDialog(CLogWnd::IDD, pParent)
+{
+	//{{AFX_DATA_INIT(CLogWnd)
+	//}}AFX_DATA_INIT
+}
+
+
+void CLogWnd::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CLogWnd)
+	//}}AFX_DATA_MAP
+}
+
+
+BEGIN_MESSAGE_MAP(CLogWnd, CDialog)
+	//{{AFX_MSG_MAP(CLogWnd)
+	ON_WM_SIZE()
+	ON_WM_CREATE()
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CLogWnd message handlers
+
+void CLogWnd::OnSize(UINT nType, int cx, int cy) 
+{
+	CDialog::OnSize(nType, cx, cy);
+
+  CWnd* p_log_list = GetDlgItem( IDC_LOG_LIST );
+  if ( ::IsWindow(*p_log_list) )
+    p_log_list->MoveWindow(0,0, cx, cy);
+}
+
+
+int CLogWnd::OnCreate(LPCREATESTRUCT lpCreateStruct) 
+{
+	if (CDialog::OnCreate(lpCreateStruct) == -1)
+		return -1;
+   
+  return 0;
+}
+
+void CLogWnd::OnSysCommand( UINT nID, LPARAM lParam )
+{
+  if (nID == SC_CLOSE )
+    ShowWindow( SW_HIDE );
+  else
+    CDialog::OnSysCommand(nID, lParam);
+}
---
+++ dirt/server/LogWnd.h
@@ -0,0 +1,48 @@
+#if !defined(AFX_LOGWND_H__5E8F16B6_AF1D_4D8F_9171_FD6C2BDC98AE__INCLUDED_)
+#define AFX_LOGWND_H__5E8F16B6_AF1D_4D8F_9171_FD6C2BDC98AE__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+// LogWnd.h : header file
+//
+
+/////////////////////////////////////////////////////////////////////////////
+// CLogWnd dialog
+
+class CLogWnd : public CDialog
+{
+// Construction
+public:
+	CLogWnd(CWnd* pParent = NULL);   // standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CLogWnd)
+	enum { IDD = IDD_DIALOG_LOG };
+	//}}AFX_DATA
+
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CLogWnd)
+	public:
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+
+	// Generated message map functions
+	//{{AFX_MSG(CLogWnd)
+	afx_msg void OnSize(UINT nType, int cx, int cy);
+	afx_msg int OnCreate(LPCREATESTRUCT lpCreateStruct);
+  afx_msg void OnSysCommand( UINT nID, LPARAM lParam );
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_LOGWND_H__5E8F16B6_AF1D_4D8F_9171_FD6C2BDC98AE__INCLUDED_)
