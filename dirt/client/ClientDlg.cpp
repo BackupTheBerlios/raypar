4f319bf876d05c8480458f4c05682d5041c0022d
tonic <tonic>
2004-01-15 21:38:37 +0000

    Working rendering test in ClientDlg.cpp

--- dirt/client/ClientDlg.cpp
+++ dirt/client/ClientDlg.cpp
@@ -29,7 +29,6 @@
 #include "geometry.h"
 #include "TestDialog.h"
 
-
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #undef THIS_FILE
@@ -219,69 +218,67 @@ HCURSOR CClientDlg::OnQueryDragIcon()
 void CClientDlg::OnButtonTest() 
 {
   //KIRILL: Temporal button and function for testing of some features
-  
-  int  imgWidth = 200, imgHeight = 200;
-	
-  ASSERT( imgWidth % 2 == 0 ); //Width must be EVEN for my realization to work!!!
+	int  imgWidth = 200, imgHeight = 200;
 
-  COLORREF * img = new COLORREF[imgWidth * imgHeight]; 
+	COLORREF * img = new COLORREF[imgWidth * imgHeight]; 
 
-  Environment		scene;
+	Environment		scene;
 	SimpleTracer	testedTracer;
-
-	CVector			sphereCenter(0,0,5);
-	Solid			* solidObject = new CSphere(sphereCenter, 1);
-	Light			* lightSource = new Light( 1.0, 1.0, 1.0, 500, 500 ,500);
+	CVector			sphereCenter(0,0,-5), sphereCenter2(1,0,-5);
+	CSphere			solidObject(sphereCenter, 2);
+	CSphere			solidObject2(sphereCenter2, 2);
+	Light			lightSource( 1.0, 1.0, 1.0, 0, 0, -2);
+	Light			lightSource2( 1.0, 0, 0, 1, 1, -2);
+	Light			lightSource3( 0, 1.0, 0, 1, -1, -2);
+	Light			lightSource4( 0, 0, 1.0, 1, 0, -2);
 	CVector			planePoint;
 	CVector			origin(0,0,0);
+	SimpleTracer	tracer;
+	CVector			color;
 	Medium			medium;
-	Tracer			* tracer = new SimpleTracer();
+	//CImage			image;
+	//image.Create( imgWidth, imgHeight, 24, 0);
 
-	medium.Betta = 1;
+	medium.Betta = 0;
 	medium.nRefr = 1;
 
-	planePoint.z = 1;
-
-	scene.Add( solidObject );
-	scene.Add( lightSource );
-
-  for( int i = 0; i < imgWidth; i ++)
+	planePoint.z = -1;
+//	planePoint.x = 0;
+//	planePoint.y = 0;
+	
+	scene.Add( &solidObject );
+	scene.Add( &solidObject2 );
+	scene.Add( &lightSource );
+	scene.Add( &lightSource2 );
+	scene.Add( &lightSource3 );
+	scene.Add( &lightSource4 );
+	
+//	Ray ray( &origin, &planePoint );
+//	tracer.trace( &medium, &ray, &scene, 1, &color);		
+//	return;
+	for( int i = 0; i < imgWidth; i ++)
 		for (int j = 0; j < imgHeight; j++)
 		{
-			CVector color;
-			
-			planePoint.x = ((double) i)/imgWidth/2 - 1.0;  
-			planePoint.y = ((double) j)/imgHeight/2 - 1.0;
-
+			planePoint.x = ((double) i)/imgWidth - 0.5;
+			planePoint.y = ((double) j)/imgHeight - 0.5;
+	
 			Ray ray( origin, planePoint );
-			tracer->trace( medium, ray, scene, 1, color);
-			
-      //It's time to create global EPS :)))
-			#define EPS (1E-7)      
-      ASSERT( ( -EPS < color.x ) && (color.x < 1+EPS ) );//must hold 
-      ASSERT( ( -EPS < color.y ) && ( color.y < 1+EPS ) );
-      ASSERT( ( -EPS < color.z ) && ( color.z < 1+EPS ) );
+			tracer.trace( medium, ray, scene, 1, color);
+	//		image.SetPixelRGB( i, imgHeight - j - 1, (char) (color.x*255.0), (char) (color.y*255.0), (char) (color.z*255.0));
+			img[i+j*imgWidth] = RGB((char) (color.x*255.0), (char) (color.y*255.0), (char) (color.z*255.0)); //Exactly this order!
 
-      ASSERT ( color.x < 0.001 );
-
-			BYTE c_red = BYTE (color.x*255.0);
-			BYTE c_green = (char) (color.y*255.0);
-			BYTE c_blue = (char) (color.z*255.0);
-			
-      img[i+j*imgWidth] = RGB(c_blue, c_green, c_red); //Exactly this order!
 		};
-
-  for (int j=0; j<imgHeight-1; j++ )
+	//image.Save( "out.bmp");
+	 for (int j=0; j<imgHeight-1; j++ )
     img[imgWidth*j]=RGB(255,0,0);
   
   CTestDialog test_dlg( imgWidth, imgHeight, img);
   test_dlg.DoModal();
 
 	delete[] img;
-	delete solidObject;
-	delete lightSource;
-	delete tracer;
+
 }
+
 
 void CClientDlg::OnButtonStart() 
 {
