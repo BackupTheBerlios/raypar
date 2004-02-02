96523487296b6d4f5aa811cbe77b4ea290e9a78a
vader <vader>
2004-02-02 00:45:33 +0000

    no message

--- dirt/server/ChildView.cpp
+++ dirt/server/ChildView.cpp
@@ -61,26 +61,41 @@ void CChildView::OnPaint()
 	CPaintDC dc(this); // device context for painting
 	
   if( m_bitmap_bits ){
-    CBitmap bmp;
-    int ret = bmp.CreateCompatibleBitmap( &dc, m_bitmap_width, m_bitmap_height );
-    ASSERT( ret );
+//    CBitmap bmp;
+//    int ret = bmp.CreateCompatibleBitmap( &dc, m_bitmap_width, m_bitmap_height );
+//    ASSERT( ret );
+//  
+//    ret = bmp.SetBitmapBits( m_bitmap_width*m_bitmap_height*sizeof(COLORREF), m_bitmap_bits );
+//    ASSERT( ret == m_bitmap_width*m_bitmap_height*(int)sizeof(COLORREF) );
   
-    ret = bmp.SetBitmapBits( m_bitmap_width*m_bitmap_height*sizeof(COLORREF), m_bitmap_bits );
-    ASSERT( ret == m_bitmap_width*m_bitmap_height*(int)sizeof(COLORREF) );
-  
-    CDC memDC;
-    memDC.CreateCompatibleDC( &dc );
-  
-    CBitmap* prev_bmp = memDC.SelectObject( &bmp );
-  
-    ret = dc.BitBlt(0, 0, m_bitmap_width, m_bitmap_height, &memDC, 0, 0, SRCCOPY );
-    if ( !ret ){  
-      ErrorMessage( "BitBlt: %s",  
-        (LPCSTR)  GetErrorMessageByErrorCode(GetLastError()) );
-    }
-    // ASSERT( ret );
-  
-    memDC.SelectObject( prev_bmp  );
+      BITMAPINFO lpbmi;
+      lpbmi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+      lpbmi.bmiHeader.biWidth = m_bitmap_width;
+      lpbmi.bmiHeader.biHeight = m_bitmap_height;
+      lpbmi.bmiHeader.biPlanes = 1;
+      lpbmi.bmiHeader.biBitCount = 32;
+      lpbmi.bmiHeader.biCompression = BI_RGB;
+      lpbmi.bmiHeader.biSizeImage = m_bitmap_width * m_bitmap_height * 4;
+      lpbmi.bmiHeader.biXPelsPerMeter = 0;
+      lpbmi.bmiHeader.biYPelsPerMeter = 0;
+      lpbmi.bmiHeader.biClrImportant = 0;
+      lpbmi.bmiHeader.biClrUsed = 0;
+
+      SetDIBitsToDevice(dc,0,0,m_bitmap_width, m_bitmap_height,0 ,0 ,0 ,m_bitmap_height ,m_bitmap_bits, &lpbmi ,DIB_PAL_COLORS);
+    
+//    CDC memDC;
+//    memDC.CreateCompatibleDC( &dc );
+//  
+//    CBitmap* prev_bmp = memDC.SelectObject( &bmp );
+//  
+//    ret = dc.BitBlt(0, 0, m_bitmap_width, m_bitmap_height, &memDC, 0, 0, SRCCOPY );
+//    if ( !ret ){  
+//      ErrorMessage( "BitBlt: %s",  
+//        (LPCSTR)  GetErrorMessageByErrorCode(GetLastError()) );
+//   }
+//    // ASSERT( ret );
+//  
+//    memDC.SelectObject( prev_bmp  );
   }
 	
 	// Do not call CWnd::OnPaint() for painting messages
